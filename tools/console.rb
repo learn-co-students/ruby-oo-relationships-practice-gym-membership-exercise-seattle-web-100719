32f33510b9366b722c076a842b3ee9cd1f0b2d98
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-14 17:43:49 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,18 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+
+
+mike = Lifter.new("Mike",125)
+john = Lifter.new("John",166)
+khon = Lifter.new("Khon",100)
+seattle = Gym.new("Seattle")
+redmond= Gym.new("Redmond")
+bellvue = Gym.new("bellvue")
+Membership.new(mike,seattle,400)
+Membership.new(john,bellvue,500)
+Membership.new(mike,bellvue,600)
+
 binding.pry
 
 puts "Gains!"
